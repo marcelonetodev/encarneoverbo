4603171edb3cc64a439a5a1b13ef468809b53a33
Marcelo Neto <36682078+celoneto@users.noreply.github.com>
2025-02-25 17:44:41 +0000

    Criação do select da area lateral

--- src/components/templates/AreaLateral.svelte
+++ src/components/templates/AreaLateral.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
   import Menu from "./Menu.svelte";
   import MenuItem from "./MenuItem.svelte";
 </script>
@@ -95,11 +95,163 @@
   <Menu titulo="Apocalipse">
     <MenuItem texto="Apocalipse" href="/livros/REV" />
   </Menu>
-  <!-- <Menu titulo="Outras categorias">
-    <MenuItem texto="Ultimas notícias" href="/" />
-    <MenuItem texto="Séries populares" href="/" />
-    <MenuItem texto="Facebook" href="/" />
-    <MenuItem texto="Instagram" href="/" />
-    <MenuItem texto="Youtube" href="/" />
-  </Menu> -->
+</aside> -->
+
+<script lang="ts">
+  import Menu from "./Menu.svelte";
+  import MenuItem from "./MenuItem.svelte";
+  function handleSelectChange(event: any) {
+    const selectedValue = event.target.value;
+    if (selectedValue) {
+      window.location.href = selectedValue;
+    }
+  }
+</script>
+
+<aside
+  class="flex flex-col self-start gap-10 min-w-72 border border-zinc-800 p-5 rounded-md"
+>
+  <Menu titulo="Navegação">
+    <MenuItem texto="Bíblia" href="/" />
+    <MenuItem texto="Estudo" href="/livros" />
+    <MenuItem texto="Notas" href="/anotacoes" />
+  </Menu>
+
+  <Menu titulo="Pentateuco">
+    <select
+      class="w-full bg-black text-white border border-zinc-800 p-2 rounded-md"
+      on:change={handleSelectChange}
+    >
+      <option value="/livros/GEN">Gênesis</option>
+      <option value="/livros/EXO">Êxodo</option>
+      <option value="/livros/LEV">Levítico</option>
+      <option value="/livros/NUM">Números</option>
+      <option value="/livros/DEU">Deuteronômio</option>
+    </select>
+  </Menu>
+
+  <Menu titulo="Históricos">
+    <select
+      class="w-full bg-black text-white border border-zinc-800 p-2 rounded-md"
+      on:change={handleSelectChange}
+    >
+      <option value="/livros/JOS">Josué</option>
+      <option value="/livros/JDG">Juízes</option>
+      <option value="/livros/RUT">Rute</option>
+      <option value="/livros/1SA">I Samuel</option>
+      <option value="/livros/2SA">II Samuel</option>
+      <option value="/livros/1KI">I Reis</option>
+      <option value="/livros/2KI">II Reis</option>
+      <option value="/livros/1CH">I Crônicas</option>
+      <option value="/livros/2CH">II Crônicas</option>
+      <option value="/livros/EZR">Esdras</option>
+      <option value="/livros/NEH">Neemias</option>
+      <option value="/livros/EST">Ester</option>
+    </select>
+  </Menu>
+
+  <Menu titulo="Poéticos">
+    <select
+      class="w-full bg-black text-white border border-zinc-800 p-2 rounded-md"
+      on:change={handleSelectChange}
+    >
+      <option value="/livros/JOB">Jó</option>
+      <option value="/livros/PSA">Salmos</option>
+      <option value="/livros/PRO">Provérbios</option>
+      <option value="/livros/ECC">Eclesiastes</option>
+      <option value="/livros/SNG">Cantares</option>
+    </select>
+  </Menu>
+
+  <Menu titulo="Proféticos">
+    <select
+      class="w-full bg-black text-white border border-zinc-800 p-2 rounded-md"
+      on:change={handleSelectChange}
+    >
+      <option value="/livros/ISA">Isaías</option>
+      <option value="/livros/JER">Jeremias</option>
+      <option value="/livros/LAM">Lamentações</option>
+      <option value="/livros/EZK">Ezequiel</option>
+      <option value="/livros/DAN">Daniel</option>
+      <option value="/livros/HOS">Oseias</option>
+      <option value="/livros/JOL">Joel</option>
+      <option value="/livros/AMO">Amós</option>
+      <option value="/livros/OBA">Obadias</option>
+      <option value="/livros/JON">Jonas</option>
+      <option value="/livros/MIC">Miqueias</option>
+      <option value="/livros/NAM">Naum</option>
+      <option value="/livros/HAB">Habacuque</option>
+      <option value="/livros/ZEP">Sofonias</option>
+      <option value="/livros/HAG">Ageu</option>
+      <option value="/livros/ZEC">Zacarias</option>
+      <option value="/livros/MAL">Malaquias</option>
+    </select>
+  </Menu>
+
+  <Menu titulo="Evangelhos">
+    <select
+      class="w-full bg-black text-white border border-zinc-800 p-2 rounded-md"
+      on:change={handleSelectChange}
+    >
+      <option value="/livros/MAT">Mateus</option>
+      <option value="/livros/MRK">Marcos</option>
+      <option value="/livros/LUK">Lucas</option>
+      <option value="/livros/JHN">João</option>
+    </select>
+  </Menu>
+
+  <Menu titulo="Atos dos Apóstolos">
+    <select
+      class="w-full bg-black text-white border border-zinc-800 p-2 rounded-md"
+      on:change={handleSelectChange}
+    >
+      <option value="/livros/ACT">Atos dos Apóstolos</option>
+    </select>
+  </Menu>
+
+  <Menu titulo="Epístolas de Paulo">
+    <select
+      class="w-full bg-black text-white border border-zinc-800 p-2 rounded-md"
+      on:change={handleSelectChange}
+    >
+      <option value="/livros/ROM">Romanos</option>
+      <option value="/livros/1CO">I Coríntios</option>
+      <option value="/livros/2CO">II Coríntios</option>
+      <option value="/livros/GAL">Gálatas</option>
+      <option value="/livros/EPH">Efésios</option>
+      <option value="/livros/PHP">Filipenses</option>
+      <option value="/livros/COL">Colossenses</option>
+      <option value="/livros/1TH">I Tessalonicenses</option>
+      <option value="/livros/2TH">II Tessalonicenses</option>
+      <option value="/livros/1TI">I Timóteo</option>
+      <option value="/livros/2TI">II Timóteo</option>
+      <option value="/livros/TIT">Tito</option>
+      <option value="/livros/PHM">Filemon</option>
+      <option value="/livros/HEB">Hebreus</option>
+    </select>
+  </Menu>
+
+  <Menu titulo="Epístolas Gerais">
+    <select
+      class="w-full bg-black text-white border border-zinc-800 p-2 rounded-md"
+      on:change={handleSelectChange}
+    >
+      <option value="/livros/JAS">Tiago</option>
+      <option value="/livros/1PE">I Pedro</option>
+      <option value="/livros/2PE">II Pedro</option>
+      <option value="/livros/1JN">I João</option>
+      <option value="/livros/2JN">II João</option>
+      <option value="/livros/3JN">III João</option>
+      <option value="/livros/JUD">Judas</option>
+    </select>
+  </Menu>
+
+  <Menu titulo="Apocalipse">
+    <select
+      class="w-full bg-black text-white border border-zinc-800 p-2 rounded-md"
+      on:change={handleSelectChange}
+    >
+      <option value="/livros/REV">Apocalipse</option>
+    </select>
+  </Menu>
 </aside>
