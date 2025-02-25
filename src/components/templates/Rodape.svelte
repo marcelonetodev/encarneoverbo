1383d7b91eed149c69108bc14a21bec4d0f73404
Marcelo Neto <36682078+celoneto@users.noreply.github.com>
2025-02-25 04:22:44 +0000

    Correção no rodapé

--- src/components/templates/Rodape.svelte
+++ src/components/templates/Rodape.svelte
@@ -14,7 +14,7 @@
     </p>
 
     <h3 class="text-zinc-700">
-      Todos os direitos reservados &copy Marcelo N. | {new Date().getFullYear()}
+      Todos os direitos reservados &copy MPN | {new Date().getFullYear()}
     </h3>
   </div>
 </footer>
