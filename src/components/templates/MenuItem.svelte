6bc5fef140341afbf05c8db2b03c72be2973e5a7
marcelonetodev <marcelo.neto.dev@gmail.com>
2025-03-08 23:16:55 +0000

    Correção de cores

--- src/components/templates/MenuItem.svelte
+++ src/components/templates/MenuItem.svelte
@@ -9,7 +9,7 @@
 <li class="w-full">
   <a
     {href}
-    class="underline-offset-8 flex gap-2 items-center justify-center lg:justify-start cursor-pointer px-2 py-2 hover:underline hover:text-white text-zinc-600 hover:bg-zinc-950 lg:rounded-sm rounded-t-sm w-full"
+    class="underline-offset-8 flex gap-2 items-center justify-center lg:justify-start cursor-pointer px-2 py-2 hover:underline hover:text-blue-950 text-zinc-600 hover:bg-zinc-950 lg:rounded-sm rounded-t-sm w-full"
     >
     <!-- on:click={() => active = true} -->
     {#if texto}
@@ -17,9 +17,9 @@
     {/if}
     {#if icone}
       <div
-        class="relative inline-flex  items-center justify-center w-10 h-10 overflow-hidden bg-blue-950/40 rounded-md"
+        class="relative inline-flex  items-center justify-center w-10 h-10 overflow-hidden "
       >
-        <span class="font-medium text-gray-600 dark:text-gray-300">
+        <span class="font-medium text-gray-600 dark:text-zinc-600 hover:text-blue-900">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
