2f01586e8b8f4b7002ec8d6eab1d25c84c2095dc
marcelonetodev <marcelo.neto.dev@gmail.com>
2025-03-12 23:33:20 +0000

    Responsividade dos cartões

--- src/components/shared/Livraria.svelte
+++ src/components/shared/Livraria.svelte
@@ -1,31 +1,28 @@
 <script lang="ts">
     import livraria from "../../constants/livraria/livraria.json";
-    import Citacao from "./Citacao.svelte";
     import Titulo from "./Titulo.svelte";
 </script>
 
-<Citacao />
-
 {#if livraria}
     <div
-        class="md:grid sm:flex sm:flex-col md:grid-cols-3 lg:grid-cols-4 gap-5 px-10 md:px-5"
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-5 px-10 md:p-5 justify-items-center"
     >
         {#each livraria as lv}
             <a
                 href={lv.link_compra}
                 target="_blank"
-                class="flex flex-col gap-5 my-5 md:my-0 rounded-sm dark:bg-zinc-900 bg-zinc-300 p-5"
+                class="flex flex-col gap-5 my-5 md:my-0 rounded-sm dark:bg-zinc-800/50 bg-zinc-300 p-5 max-w-80"
             >
                 <img
                     src={lv.imagem}
                     alt="Imagem do livro"
-                    class="h-60 lg:h-80 md:h80 object-fill overflow-hidden rounded-sm"
+                    class="h-auto object-fill overflow-hidden rounded-sm"
                 />
                 <Titulo principalMenor={lv.título} secundario={lv.autor} />
             </a>
         {/each}
     </div>
-    <Titulo
-        secundario="Você será redirecionado para o site da amazon clicando no livro desejado"
-    />
+    <p class="text-base text-zinc-500 text-center">
+        Você será redirecionado para o site da amazon clicando no livro desejado
+    </p>
 {/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import "../app.css";
   import Pagina from "../components/templates/Pagina.svelte";
-  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
-
-  injectSpeedInsights();
   let { children } = $props();
 </script>
 
@@ -15,7 +12,10 @@
   />
   <meta name="robots" content="index, follow" />
   <meta property="og:title" content="Encarne o Verbo" />
-  <meta property="og:description" content="No princípio era o Verbo, e o Verbo estava com Deus, e o Verbo era Deus." />
+  <meta
+    property="og:description"
+    content="No princípio era o Verbo, e o Verbo estava com Deus, e o Verbo era Deus."
+  />
   <meta property="og:image" content="./logo.png" />
   <meta
     name="viewport"
