308919d825128c876447d18ecca41c1da50463ca
marcelonetodev <marcelo.neto.dev@gmail.com>
2025-03-12 15:38:50 +0000

    Remoção dos links

--- src/components/shared/Livraria.svelte
+++ src/components/shared/Livraria.svelte
@@ -11,9 +11,9 @@
         class="md:grid sm:flex sm:flex-col md:grid-cols-3 lg:grid-cols-4 gap-5 px-10 md:px-5"
     >
         {#each livraria as lv}
-            <a
-                href={lv.link_compra}
-                target="_blank"
+            <!-- href={lv.link_compra}
+        target="_blank" -->
+            <div
                 class="flex flex-col gap-5 my-5 md:my-0 rounded-sm dark:bg-zinc-900 bg-zinc-300 p-5"
             >
                 <img
@@ -22,7 +22,7 @@
                     class="h-60 lg:h-80 md:h80 object-fill overflow-hidden rounded-sm"
                 />
                 <Titulo principalMenor={lv.título} secundario={lv.autor} />
-            </a>
+            </div>
         {/each}
     </div>
     <Titulo
